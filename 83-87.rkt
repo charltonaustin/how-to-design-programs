6b916d2b3857408d3b67cb3d783b5193a5bc9cf3
charltonaustin <charlton.austin@kin.com>
2024-07-11 05:42:52 -0700

    Adding in some work from last night.

--- 83-87.rkt
+++ 83-87.rkt
@@ -10,10 +10,11 @@
 ; the cursor displayed between s and t
 
 
-(define (render editor)
-  (overlay/align "left" "center"
-               (beside (text (editor-pre editor) 11 "black") (rectangle 1 16 "solid" "black") (text (editor-post editor) 11 "black"))
-               (empty-scene 200 20)))
 
-(define (edit editor keybinding)
-  editor)
+(overlay/align "left" "center"
+               (beside (text "hello world " 11 "black") (rectangle 1 16 "solid" "black") (text "hello world" 11 "black"))
+               (empty-scene 200 20))
+
+
+(define (edit ed ke)
+  ed)
