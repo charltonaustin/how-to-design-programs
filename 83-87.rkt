be7a1e988efdeef41f9e18350c74c03fae4faf22
charltonaustin <charlton.austin@kin.com>
2024-07-11 20:27:12 -0700

    Adding in a few new funcitons for a text editor.

--- 83-87.rkt
+++ 83-87.rkt
@@ -1,8 +1,9 @@
 #lang racket
+
 (require 2htdp/image)
 (require 2htdp/universe)
 
-(define-struct editor [pre post])
+(define-struct editor [pre post cursor])
 ; An Editor is a structure:
 ;   (make-editor String String)
 ; interpretation (make-editor s t) describes an editor
@@ -11,10 +12,32 @@
 
 
 
-(overlay/align "left" "center"
-               (beside (text "hello world " 11 "black") (rectangle 1 16 "solid" "black") (text "hello world" 11 "black"))
-               (empty-scene 200 20))
+(define (render editor)
+  (overlay/align
+   "left" "center"
+   (beside
+    (text (editor-pre editor) 11 "black")
+    (editor-cursor editor)
+    (text (editor-post editor) 11 "black"))
+   (empty-scene 200 20)))
 
+(define (delete-previous editor)
+  (if (> (editor-pre edit)))
+  (make-editor (substring (editor-pre editor) 0 (string-length (editor-pre editor))))
 
-(define (edit ed ke)
-  ed)
+(define (append-previous editor)
+  editor)
+
+(define (move-left editor) editor)
+
+(define (move-right editor) editor)
+
+(define (edit editor keyevent)
+  (if (not (= (string-length keyevent) 1))
+      editor
+      (cond 
+        [(string=? "\r") editor]
+        [(string=? "right") (move-right editor)]
+        [(string=? "left") (move-left editor)]
+        [(string=? "\\d") (delete-previous editor)]
+        [else (append-previous editor)])))
